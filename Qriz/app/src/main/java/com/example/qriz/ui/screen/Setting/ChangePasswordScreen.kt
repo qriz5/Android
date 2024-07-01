aa3b62797f09d9790800b257834af4aee6f8de14
I-MO <know0329@naver.com>
2024-07-01 21:45:23 +0900

    fix : 비밀번호변경 화면 색상 수정

--- Qriz/app/src/main/java/com/example/qriz/ui/screen/Setting/ChangePasswordScreen.kt
+++ Qriz/app/src/main/java/com/example/qriz/ui/screen/Setting/ChangePasswordScreen.kt
@@ -10,6 +10,7 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.imePadding
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
@@ -54,7 +55,7 @@ fun ChangePasswordScreen(){
     }
     Surface(modifier = Modifier
         .fillMaxSize()
-        .padding(start = 18.dp, end = 18.dp, top = 16.dp, bottom = 40.dp)) {
+        .padding(start = 18.dp, end = 18.dp, top = 16.dp, bottom = 40.dp).background(Color(0xFFF8F9FE)).imePadding()) {
         Column {
             Column(Modifier.weight(1f)){
                 Row(modifier = Modifier
@@ -65,33 +66,32 @@ fun ChangePasswordScreen(){
                     Spacer(modifier = Modifier.width(1.dp))
                 }
                 Spacer(modifier = Modifier.height(32.dp))
-                Text(text = "소중한 정보를 보호하기 위해\n새로운 비밀번호로 변경해 주세요!", fontSize = 20.sp, fontFamily = appleNeo, fontWeight = FontWeight.Bold, color = Color(0xFF545454))
+                Text(text = "소중한 정보를 보호하기 위해\n새로운 비밀번호로 변경해 주세요!", fontSize = 20.sp, fontFamily = appleNeo, fontWeight = FontWeight.Bold)
                 Spacer(modifier = Modifier.height(16.dp))
-                Text(text = "· 다른 아이디/사이트에서 사용한 적 없는 비밀번호", fontSize = 14.sp, fontFamily = appleNeo, color = Color(0xFF545454))
-                Text(text = "· 이전에 사용한 적 없는 비밀번호가 안전합니다.", fontSize = 14.sp, fontFamily = appleNeo, color = Color(0xFF545454))
-                Spacer(modifier = Modifier.height(40.dp))
+                Text(text = "이전에 사용한 적 없는 비밀번호가 안전합니다.", fontSize = 16.sp, fontFamily = appleNeo, color = Color(0xFF8F9AAA))
+                Spacer(modifier = Modifier.height(24.dp))
                 TextBox(modifier = Modifier
                     .fillMaxWidth()
                     .height(48.dp)
-                    .background(color = textFieldColor, shape = RoundedCornerShape(12.dp)), innerBoxModifier = Modifier.padding(start = 16.dp), hint = "현재 비밀번호", value = currentPassword, onValueChange = {currentPassword = it})
+                    .background(color = Color(0xFFEEF0F8), shape = RoundedCornerShape(12.dp)), innerBoxModifier = Modifier.padding(start = 16.dp), hint = "현재 비밀번호", value = currentPassword, onValueChange = {currentPassword = it})
                 Spacer(modifier = Modifier.height(8.dp))
-                Text(text = "사용중인 비밀번호를 입력해주세요.", fontSize = 12.sp, fontFamily = appleNeo, color = Color(0xFF6C6D6C))
-                Spacer(modifier = Modifier.height(16.dp))
-                TextBox(modifier = Modifier
-                    .fillMaxWidth()
-                    .height(48.dp)
-                    .background(color = textFieldColor, shape = RoundedCornerShape(12.dp)), innerBoxModifier = Modifier.padding(start = 16.dp), hint = "새 비밀번호", value = newPassword, onValueChange = {newPassword = it})
-                Spacer(modifier = Modifier.height(8.dp))
-                Text(text = "새 비밀번호를 입력해주세요.(영문,숫자,특수문자 8~20자)", fontSize = 12.sp, fontFamily = appleNeo, color = Color(0xFF6C6D6C))
+                Text(text = "사용중인 비밀번호를 입력해주세요.", fontSize = 12.sp, fontFamily = appleNeo, color = Color(0xFFEF5D5D))
                 Spacer(modifier = Modifier.height(16.dp))
                 TextBox(modifier = Modifier
                     .fillMaxWidth()
                     .height(48.dp)
-                    .background(color = textFieldColor, shape = RoundedCornerShape(12.dp)), innerBoxModifier = Modifier.padding(start = 16.dp), hint = "새 비밀번호 확인", value = newPassword, onValueChange = {newPassword = it})
+                    .background(color = Color(0xFFEEF0F8), shape = RoundedCornerShape(12.dp)), innerBoxModifier = Modifier.padding(start = 16.dp), hint = "새 비밀번호", value = newPassword, onValueChange = {newPassword = it})
                 Spacer(modifier = Modifier.height(8.dp))
-                Text(text = "새 비밀번호를 한번 더 입력해주세요.", fontSize = 12.sp, fontFamily = appleNeo, color = Color(0xFF6C6D6C))
+                Text(text = "새 비밀번호를 입력해주세요.(영문,숫자,특수문자 8~20자)", fontSize = 12.sp, fontFamily = appleNeo, color = Color(0xFFEF5D5D))
+                Spacer(modifier = Modifier.height(16.dp))
+                TextBox(modifier = Modifier
+                    .fillMaxWidth()
+                    .height(48.dp)
+                    .background(color = Color(0xFFEEF0F8), shape = RoundedCornerShape(12.dp)), innerBoxModifier = Modifier.padding(start = 16.dp), hint = "새 비밀번호 확인", value = newPassword, onValueChange = {newPassword = it})
+                Spacer(modifier = Modifier.height(8.dp))
+                Text(text = "새 비밀번호를 한번 더 입력해주세요.", fontSize = 12.sp, fontFamily = appleNeo, color = Color(0xFFEF5D5D))
             }
-            Text(modifier = Modifier.fillMaxWidth().padding(bottom = 32.dp), text = "다음에 변경하기", color =  Color(0xFFA0A0A0),textAlign = TextAlign.Center, textDecoration = TextDecoration.Underline)
+            Text(modifier = Modifier.fillMaxWidth().padding(bottom = 32.dp), text = "비밀번호를 잊으셨나요?", color =  Color(0xFFA0A0A0),textAlign = TextAlign.Center, textDecoration = TextDecoration.Underline)
             Button(
                 onClick = { },
                 modifier = Modifier
